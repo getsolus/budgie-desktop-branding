84c3df0fee0559a1dd897010740f6da8e6844197
Joshua Strobl <joshua@stroblindustries.com>
2019-03-17 16:02:40 +0200

    Bump to 20.0. Fixes lack of newlines in some schema entries. Use os-installer in default for livecd schema.

--- mkrelease.sh
+++ mkrelease.sh
@@ -5,7 +5,7 @@ git submodule init
 git submodule update
 
 # Script for ikey because he went with meson. *shrug*
-VERSION="19"
+VERSION="20"
 NAME="budgie-desktop-branding"
 git-archive-all --format tar --prefix ${NAME}-${VERSION}/ --verbose -t HEAD ${NAME}-${VERSION}.tar
 xz -9 "${NAME}-${VERSION}.tar"
